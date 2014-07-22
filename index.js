7f20251a5c42bf7fe9ff489e42b84f37c8ef1e31
Gustavo Henke <gustavo@injoin.com.br>
2014-07-22 00:41:15 -0300

    feat(response): deal with mime types
    Now, we'll reply with the correct mime type depending on the
    request file. For this, we'll be using the mime module, and whenever
    it fails, we'll use the mime type coming from GitHub, which usually
    is text/plain or application/octet-stream, so we don't have to read the
    file on our side and try to identify if it's text or binary.
    
    Closes #2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var https = require( "https" );
 var bower = require( "bower" );
+var mime = require( "mime" );
 
 // Express and middlewares
 var express = require( "express" );
@@ -10,6 +11,10 @@ var compression = require( "compression" );
 // -------------------------------------------------------------------------------------------------
 
 var app = express();
+
+// Set the default mime type to an object, so we know that it didn't found anything that could
+// conflict with a valid mimetype.
+mime.default_type = {};
 
 app.use( compression({
     threshold: 1024
@@ -31,7 +36,10 @@ app.route( "/:package/:version/*" ).get(function( req, res ) {
         url += req.params[ 0 ];
 
         https.get( url, function( ghResp ) {
+            var type = findMimetype( url, ghResp );
+
             res.statusCode = ghResp.statusCode;
+            res.set( "Content-Type", type );
             res.set( "X-Expanded-Url", url );
 
             ghResp.pipe( res );
@@ -54,4 +62,13 @@ function getSlug( url ) {
 
 function getGithubUrl( slug ) {
     return "https://raw.githubusercontent.com/" + slug + "/";
+}
+
+function findMimetype( url, ghResp ) {
+    var type = mime.lookup( url );
+
+    // If the defaule mime type was used, this means we should rely on GitHub and return their
+    // content-type (which is usually plain text or octet-stream).
+    // Otherwise, we simply use the mime module returned value.
+    return type === mime.default_type ? ghResp.headers[ "content-type" ] : type;
 }
