5cff17ed0c064ac8fd0097b6a901e21cd4e44e7f
Gustavo Henke <gustavo@injoin.io>
2014-08-29 08:17:26 -0300

    chore(test): create global for sinon and bower
    To allow easier suite setup.

--- test/unit/bootstrap.js
+++ test/unit/bootstrap.js
@@ -1,7 +1,11 @@
 "use strict";
 
+var bower = require( "bower" );
 var chai = require( "chai" );
 var chaiAsPromised = require( "chai-as-promised" );
+var sinon = require( "sinon" );
 
 chai.use( chaiAsPromised );
+global.bower = bower;
 global.expect = chai.expect;
+global.sinon = sinon;
