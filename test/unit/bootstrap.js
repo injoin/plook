aa1cfd3198d509fed3f8d410dcc40844a3094576
Gustavo Henke <gustavo@injoin.io>
2014-08-31 22:24:37 -0300

    fix(tests): don't redefine sinon and bower
    To make JSHint happier, let's not "redefine" sinon and bower
    vars.

--- test/unit/bootstrap.js
+++ test/unit/bootstrap.js
@@ -1,11 +1,9 @@
 "use strict";
 
-var bower = require( "bower" );
 var chai = require( "chai" );
 var chaiAsPromised = require( "chai-as-promised" );
-var sinon = require( "sinon" );
 
 chai.use( chaiAsPromised );
-global.bower = bower;
 global.expect = chai.expect;
-global.sinon = sinon;
+global.bower = require( "bower" );
+global.sinon = require( "sinon" );
