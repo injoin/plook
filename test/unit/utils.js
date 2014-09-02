064a3e265b52f1c1a0a34218f0b761d2e9013df0
Gustavo Henke <gustavo@injoin.io>
2014-09-02 00:07:08 -0300

    feat(utils): add .extend() method
    For quickly extending a destiny object with various source objects.

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -24,4 +24,31 @@ describe( "utils", function() {
             expect( actual ).to.equal( "https://raw.githubusercontent.com/foo/bar/1.0.0/baz.js" );
         });
     });
+
+    // ---------------------------------------------------------------------------------------------
+
+    describe( ".extend()", function() {
+        it( "should return the input if not object", function() {
+            expect( utils.extend() ).to.be.undefined;
+            expect( utils.extend( 1 ) ).to.equal( 1 );
+            expect( utils.extend( "foo" ) ).to.equal( "foo" );
+        });
+
+        it( "should mix all source objects into destiny object", function() {
+            expect( utils.extend( { a: 1 }, { b: 2 }, { c: 3 } ) ).to.deep.equal({
+                a: 1,
+                b: 2,
+                c: 3
+            });
+        });
+
+        it( "should return destiny object", function() {
+            var dest = { a: 1 };
+            var actual = utils.extend( dest, {
+                b: 2
+            });
+
+            expect( actual ).to.equal( dest );
+        });
+    });
 });
